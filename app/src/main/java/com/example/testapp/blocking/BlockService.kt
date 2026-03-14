0e69ffcbc0994329981e8e26e27a7c94b8e03447
Gilpendrago <karankrishnatony@gmail.com>
2026-03-14 11:10:09 +0530

    Updated blocker service and accessibility config

--- app/src/main/java/com/example/testapp/blocking/BlockService.kt
+++ app/src/main/java/com/example/testapp/blocking/BlockService.kt
@@ -1,4 +1,27 @@
 package com.example.testapp.blocking
 
-class BlockService {
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityService.GLOBAL_ACTION_HOME
+import android.view.accessibility.AccessibilityEvent
+import android.widget.Toast
+
+class BlockService : AccessibilityService() {
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {
+
+        val packageName = event.packageName?.toString() ?: return
+
+        if (packageName == "com.google.android.youtube") {
+
+            Toast.makeText(
+                this,
+                "YouTube blocked. Complete tasks to unlock.",
+                Toast.LENGTH_LONG
+            ).show()
+
+            performGlobalAction(GLOBAL_ACTION_HOME)
+        }
+    }
+
+    override fun onInterrupt() {}
 }
